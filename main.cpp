db917c65fd963b518e8c531d134c344572e5f316
Julian Bensch <Bensch.Julian@gmail.com>
2020-06-15 23:35:15 +0200

    Added Mesh class and example Mesh (triangle) in main() method

--- main.cpp
+++ main.cpp
@@ -2,17 +2,27 @@
 #include <GL/glew.h>
 #include "display.h"
 #include "shader.h"
+#include "mesh.h"
 
 int main()
 {
     Display display(800, 800, "Hello World!");
+
+    Vertex verticies[] =
+        {
+            Vertex(glm::vec3(0, -1, 0)),
+            Vertex(glm::vec3(-1, 1, 0)),
+            Vertex(glm::vec3(1, 1, 0)),
+        };
+
+    Mesh mesh(verticies, sizeof(verticies)/sizeof(verticies[0]));
 
     Shader shader("./res/basicShader");
 
     while (!display.IsClosed()) {
         display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
         shader.Bind();
-
+        mesh.Draw();
 
         display.Update();
     }
